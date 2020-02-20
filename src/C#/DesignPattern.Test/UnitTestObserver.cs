0f1a8080cadbcc6ce81b794bd084aa7263ebb2f3
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-20 21:37:15 +0100

    changed tests

--- src/C#/DesignPattern.Test/UnitTestObserver.cs
+++ src/C#/DesignPattern.Test/UnitTestObserver.cs
@@ -37,6 +37,21 @@ namespace DesignPattern.Test.ObserverTest1
             subject.Notify();
             Assert.True((observer1.Notified == 2) && (observer2.Notified == 1));
         }
+
+        [Fact]
+        public void ObserverTest4()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.Notify();
+            subject.Attach(observer2);
+            subject.Notify();
+            subject.Detach(observer1);
+            subject.Notify();
+            Assert.True(observer1.Notified == observer2.Notified);
+        }
     }
 }
 
@@ -49,11 +64,9 @@ namespace DesignPattern.Test.ObserverTest2
         public void ObserverTest1()
         {
             ConcreteSubject subject = new ConcreteSubject();
-            ConcreteObserver observer = new ConcreteObserver();
-            subject.Attach(observer);
-            Assert.True(observer.Value == 0);
-            subject.ChangeNumber(5);
-            Assert.True(observer.Value == 5);
+            ConcreteObserver observer1 = new ConcreteObserver();
+            subject.Attach(observer1);
+            Assert.True(observer1.Notified == 0);
         }
 
         [Fact]
@@ -61,15 +74,9 @@ namespace DesignPattern.Test.ObserverTest2
         {
             ConcreteSubject subject = new ConcreteSubject();
             ConcreteObserver observer1 = new ConcreteObserver();
-            ConcreteObserver observer2 = new ConcreteObserver();
             subject.Attach(observer1);
-            subject.ChangeNumber(5);
-            Assert.True(observer1.Value == 5);
-            Assert.False(observer2.Value == 5);
-            subject.Attach(observer2);
-            subject.ChangeNumber(2);
-            Assert.True(observer1.Value == 2);
-            Assert.True(observer2.Value == 2);
+            subject.Notify();
+            Assert.True(observer1.Notified == 1);
         }
 
         [Fact]
@@ -79,14 +86,25 @@ namespace DesignPattern.Test.ObserverTest2
             ConcreteObserver observer1 = new ConcreteObserver();
             ConcreteObserver observer2 = new ConcreteObserver();
             subject.Attach(observer1);
-            subject.ChangeNumber(5);
-            Assert.True(observer1.Value == 5);
-            Assert.False(observer2.Value == 5);
+            subject.Notify();
             subject.Attach(observer2);
+            subject.Notify();
+            Assert.True((observer1.Notified == 2) && (observer2.Notified == 1));
+        }
+
+        [Fact]
+        public void ObserverTest4()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.Notify();
+            subject.Attach(observer2);
+            subject.Notify();
             subject.Detach(observer1);
-            subject.ChangeNumber(2);
-            Assert.False(observer1.Value == 2);
-            Assert.True(observer2.Value == 2);
+            subject.Notify();
+            Assert.True(observer1.Notified == observer2.Notified);
         }
     }
 }
@@ -100,11 +118,9 @@ namespace DesignPattern.Test.ObserverTest3
         public void ObserverTest1()
         {
             ConcreteSubject subject = new ConcreteSubject();
-            ConcreteObserver observer = new ConcreteObserver();
-            subject.Attach(observer);
-            Assert.True(observer.Value == 0);
-            subject.ChangeNumber(5);
-            Assert.True(observer.Value == 5);
+            ConcreteObserver observer1 = new ConcreteObserver();
+            subject.Attach(observer1);
+            Assert.True(observer1.Notified == 0);
         }
 
         [Fact]
@@ -112,15 +128,9 @@ namespace DesignPattern.Test.ObserverTest3
         {
             ConcreteSubject subject = new ConcreteSubject();
             ConcreteObserver observer1 = new ConcreteObserver();
-            ConcreteObserver observer2 = new ConcreteObserver();
             subject.Attach(observer1);
-            subject.ChangeNumber(5);
-            Assert.True(observer1.Value == 5);
-            Assert.False(observer2.Value == 5);
-            subject.Attach(observer2);
-            subject.ChangeNumber(2);
-            Assert.True(observer1.Value == 2);
-            Assert.True(observer2.Value == 2);
+            subject.Notify(subject);
+            Assert.True(observer1.Notified == 1);
         }
 
         [Fact]
@@ -130,14 +140,25 @@ namespace DesignPattern.Test.ObserverTest3
             ConcreteObserver observer1 = new ConcreteObserver();
             ConcreteObserver observer2 = new ConcreteObserver();
             subject.Attach(observer1);
-            subject.ChangeNumber(5);
-            Assert.True(observer1.Value == 5);
-            Assert.False(observer2.Value == 5);
+            subject.Notify(subject);
             subject.Attach(observer2);
+            subject.Notify(subject);
+            Assert.True((observer1.Notified == 2) && (observer2.Notified == 1));
+        }
+
+        [Fact]
+        public void ObserverTest4()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.Notify(subject);
+            subject.Attach(observer2);
+            subject.Notify(subject);
             subject.Detach(observer1);
-            subject.ChangeNumber(2);
-            Assert.False(observer1.Value == 2);
-            Assert.True(observer2.Value == 2);
+            subject.Notify(subject);
+            Assert.True(observer1.Notified == observer2.Notified);
         }
     }
 }
