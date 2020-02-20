9a89db01125de578393d1618dfbd96b48097a033
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-20 21:16:45 +0100

    added tests

--- src/C#/DesignPattern.Test/UnitTestObserver.cs
+++ src/C#/DesignPattern.Test/UnitTestObserver.cs
@@ -90,3 +90,54 @@ namespace DesignPattern.Test.ObserverTest2
         }
     }
 }
+
+namespace DesignPattern.Test.ObserverTest3
+{
+    using DesignPattern.Behavioral.Observer.Observer3;
+    public class UnitTestObserver1
+    {
+        [Fact]
+        public void ObserverTest1()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer = new ConcreteObserver();
+            subject.Attach(observer);
+            Assert.True(observer.Value == 0);
+            subject.ChangeNumber(5);
+            Assert.True(observer.Value == 5);
+        }
+
+        [Fact]
+        public void ObserverTest2()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.ChangeNumber(5);
+            Assert.True(observer1.Value == 5);
+            Assert.False(observer2.Value == 5);
+            subject.Attach(observer2);
+            subject.ChangeNumber(2);
+            Assert.True(observer1.Value == 2);
+            Assert.True(observer2.Value == 2);
+        }
+
+        [Fact]
+        public void ObserverTest3()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.ChangeNumber(5);
+            Assert.True(observer1.Value == 5);
+            Assert.False(observer2.Value == 5);
+            subject.Attach(observer2);
+            subject.Detach(observer1);
+            subject.ChangeNumber(2);
+            Assert.False(observer1.Value == 2);
+            Assert.True(observer2.Value == 2);
+        }
+    }
+}
