25703e0034048e61e5ef3822b8aaae253687b592
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-20 18:42:13 +0100

    added tests for first observer pattern

---
+++ src/C#/DesignPattern.Test/UnitTestObserver.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using DesignPattern.Behavioral.Observer.Observer1;
+
+namespace DesignPattern.Test
+{
+    public class UnitTestObserver1
+    {
+        [Fact]
+        public void ObserverTest1()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver1 observer1 = new ConcreteObserver();
+            subject.Attach(observer1);
+            Assert.True(observer1.Notified == 0);
+        }
+
+        [Fact]
+        public void ObserverTest2()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver1 observer1 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.Notify();
+            Assert.True(observer1.Notified == 1);
+        }
+
+        [Fact]
+        public void ObserverTest3()
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            ConcreteObserver1 observer1 = new ConcreteObserver();
+            ConcreteObserver1 observer2 = new ConcreteObserver();
+            subject.Attach(observer1);
+            subject.Notify();
+            subject.Attach(observer2);
+            subject.Notify();
+            Assert.True((observer1.Notified == 2) && (observer2.Notified == 1));
+        }
+    }
+}
--- src/C#/DesignPattern/Patterns/Behavioral/Observer/Observer1/ConcreteObserver.cs
+++ src/C#/DesignPattern/Patterns/Behavioral/Observer/Observer1/ConcreteObserver.cs
@@ -5,7 +5,7 @@ namespace DesignPattern.Behavioral.Observer.Observer1
     /// <summary>
     /// This class represents a Observer who remembers how often he got notified by the subject
     /// </summary>
-    class ConcreteObserver1 : IObserver
+    public class ConcreteObserver : IObserver
     {
         public int Notified{ get; set; } = 0;
 
