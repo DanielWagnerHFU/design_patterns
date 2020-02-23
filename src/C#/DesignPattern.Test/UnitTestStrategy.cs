82905211c6bd7a3aeaf62c32ebc1338ae270dd87
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-23 17:45:46 +0100

    added unit tests

--- src/C#/DesignPattern.Test/UnitTestStrategy.cs
+++ src/C#/DesignPattern.Test/UnitTestStrategy.cs
@@ -9,8 +9,24 @@ namespace DesignPattern.Test
         [Fact]
         public void StrategyTest1()
         {
-            Client client = new Client();
-            
+            Client client = new Client(new StrategyA());
+            Assert.Equal("AlgorithmA", client.CallAlgorithm());
+        }
+
+        [Fact]
+        public void StrategyTest2()
+        {
+            Client client = new Client(new StrategyB());
+            Assert.Equal("AlgorithmB", client.CallAlgorithm());
+        }
+
+        [Fact]
+        public void StrategyTest3()
+        {
+            Client client = new Client(new StrategyB());
+            Assert.Equal("AlgorithmB", client.CallAlgorithm());
+            client.Strategy = new StrategyA();
+            Assert.Equal("AlgorithmA", client.CallAlgorithm());
         }
     }
 }
